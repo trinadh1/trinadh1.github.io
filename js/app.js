cf67abeb1a095158689b82fa58da98a09d82aca7
TRINADH1! <reddy.trinadhm50@gmail.com>
2015-09-10 01:26:19 +0530

    added effects to portfolio

--- js/app.js
+++ js/app.js
@@ -3,13 +3,42 @@
 //   bars.reach(80, 1000);  //dest is the progress # at which you want to reach
 // });
 
+$(document).ready(function(){
 
-function portfolio() {
-$('#calci').click(function(){
+  $('#all9').click(function(){
+    $('.all').fadeIn(1500);
+  });
 
-  bootbox.alert("hi");
+  $('#psd9').click(function(){
+    $('.all').hide();
+    $('.psd').fadeIn(2000);
+  });
+
+  $('#calci9').click(function(){
+    $('.all').hide();
+    $('.calci').fadeIn(2000);
+  });
+
+  $('#puzzle9').click(function(){
+    $('.all').hide();
+    $('.puzzle').fadeIn(2000);
+  });
+
+  $('#jumbled9').click(function(){
+    $('.all').hide();
+    $('.jumbled').fadeIn(2000);
+  });
+
+  $('#psd1').click(function(){
+    bootbox.alert('<img class="img-responsive" src="../images/psd.JPG">');
+    
+  });
+
 });
-}
 
-$(document).ready(portfolio);
+// $(document).ready(portfolio);
+
+
+// }
+
 
